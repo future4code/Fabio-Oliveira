19f53753d07fdbfe4b7b369b88c95a9e931fbb17
Fábio <fabiojordao97@yahoo.com.br>
2020-10-07 15:11:28 -0300

    Fiz os exercícios conforme requisitado

---
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -0,0 +1,49 @@
+let postArray = []
+
+const clickButton = () => {
+        let title = document.getElementById("titulo-post")
+        let author = document.getElementById("autor-post")
+        let content = document.getElementById("conteudo-post")
+        let image = document.getElementById("imagem-post")
+        let box = document.getElementById("container-de-posts")
+        box.innerHTML += `<h1> ${title.value} </h1>`
+        box.innerHTML += `<h4> ${author.value} </h4>`
+        box.innerHTML += `<p> ${content.value} </p>`
+        box.innerHTML += `<img scr="${image.value}><img>`
+        
+
+        // if(image.value.includes("http")){
+        //     box.innerHTML += `<img scr="${image.value}><img>`
+    
+        //     } else{
+        //         window.alert("Manda um link, caralho!")
+        //     }
+        
+        
+
+        const post = {
+            title: title.value,
+            author: author.value,
+            content: content.value,
+            image: image.value
+        }
+
+        postArray.push(post)
+
+        console.log(post)
+        postLists = [...postArray, post]
+        console.log(postArray)
+
+        title.value = ""
+        author.value = ""
+        content.value = ""
+        image.value = ""
+    }
+    
+    // const apertouEnter = (event) => {
+    //     if (event.key === "Enter"){
+    //         clickBotao()
+    //     }
+    // }
+
+    // const nomeDoUsuario = usuario['nome'];
