293818dd50b1270431e3fe35fb92e27a0f9d2dad
Fábio <fabiojordao97@yahoo.com.br>
2020-10-09 18:07:55 -0300

    Fiz os exercícios 2 e 3 de Funções de Array.

--- semana4/lista-de-exercicios/lista-de-exercicios.js
+++ semana4/lista-de-exercicios/lista-de-exercicios.js
@@ -312,3 +312,68 @@
 // console.log(pessoasInfantis)
 
 
+// Exercício 2-
+
+// a)
+
+const array = [1,2,3,4,5,6]
+
+// const multiplicaArray = array.map( (numero) => {
+//    return numero * 2
+      
+// })
+
+// // b)
+
+// const multiplicaArrayPorTres = array.map( (numero) => {
+//     return String(numero * 3)
+
+// })
+
+// console.log(multiplicaArray)
+// console.log(multiplicaArrayPorTres)
+
+
+// // c)
+
+    const paridade = array.map( (numero) => {
+        if(numero % 2 === 0){
+            return `${numero} é par`
+        }
+        return `${numero} é ímpar`
+    })
+
+    console.log(paridade)
+
+
+// Exercício 4 -
+
+// a)
+
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
+
+// const permissaoPessoas = pessoas.filter( (pessoa) => {
+//     if(pessoa.idade > 14 && pessoa.idade < 60 && pessoa.altura > 1.5 ){
+//             return true
+//     }
+
+// })
+
+// b)
+
+// const permissaoNegadaPessoas = pessoas.filter( (pessoa) => {
+//     if(pessoa.idade < 14 || pessoa.idade > 60 || pessoa.altura < 1.5 ){
+//             return true
+//     }
+
+// })
+
+// console.log(permissaoPessoas)
+// console.log(permissaoNegadaPessoas)
