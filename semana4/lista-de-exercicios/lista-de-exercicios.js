6149459a828f368bd765a9030617e38f8ed87e7a
Fábio <fabiojordao97@yahoo.com.br>
2020-10-09 11:54:00 -0300

    Fiz os três primeiros exercícios de interpretação. Como esqueci dos commits nos anteriores, mandei tudo de uma vez. A partir do próximo irei enviar um por um.

---
+++ semana4/lista-de-exercicios/lista-de-exercicios.js
@@ -0,0 +1,21 @@
+// Exercícios de Interpretação de Código
+
+// Exercício 1 -
+
+// O código faz uma conversão de valores. Através da função, o usuário colocará um valor dentro da const cotacao e traz um retorno fazendo o cálculo do valor em dólar que está sendo informação e a cotação que será informada pelo usuário. Na const meuDinheiro, a função é chamada junto de um valor e então realiza o cálculo chamado no return. O resultado apresentado no console, considerando a cotação atual do dólar, será de 560.
+
+
+// Exercício 2 -
+
+// O código mostra o resultado de cada investimento informado no tipoDeInvestimento. O switch multiplica o valor informado pelo usuário pelo valor de cada investimento. Em caso de erro de informações, apresenta um alerta. Por fim, retorna o valor final já calculado. 
+// Na const novoMontante, o valor informado no console será de 150 * 1.1, totalizando 165.
+// Na const segundoMontante, será exibido o alert, pois Tesouro Direto não é um tipo de investimento válido.
+
+
+// Exercício 3 -
+
+// O código contém um array que passará por um loop do tipo for que irá separá-los em dois grupos que serão contidos nos arrays array1 e array2. Através do if(numero % 2 === 0), o código irá separar os números entre pares e ímpares usando o resto de divisão representado pelo símbolo %. Se os números forem pares, irão para o array1. Se não forem, irão para o array2.
+// O primeiro console.log mostra o tamanho total do array numeros, que é 14.
+// O segundo console.log mostra o tamanho do array formado apenas por números pares, que é 6.
+// O terceiro console.log mostra o tamanho do array formado apenas por números ímpares, que é 8.
+
