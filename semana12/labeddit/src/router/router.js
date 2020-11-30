b0748e023a1aaebe42954cf47be39f77c5d8959e
Fábio <fabiojordao97@yahoo.com.br>
2020-11-30 17:21:02 -0300

    Projeto iniciado.

---
+++ semana12/labeddit/src/router/router.js
@@ -0,0 +1,15 @@
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+
+const Router = () => {
+
+    return (
+        <BrowserRouter>
+        <Switch>
+            
+        </Switch>
+        </BrowserRouter>
+    )
+
+}
+
+export default Router
---
+++ semana12/labeddit/src/screens/registerPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import axios from 'axios'
+
+const RegisterPage = () => {
+
+}
+
+export default RegisterPage
