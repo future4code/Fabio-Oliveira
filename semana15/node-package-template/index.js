66f8f0a69fa496945d84d756568b3faf38b2495c
Fábio <fabiojordao97@yahoo.com.br>
2021-01-04 17:10:30 -0300

    Exercício 1 pronto.

---
+++ semana15/node-package-template/index.js
@@ -0,0 +1,16 @@
+// 1 -
+// a)
+
+// b)
+const name = "Fábio";
+const age = Number(23);
+
+console.log(`Olá, ${name}! Você tem ${age} anos.`)
+
+const newAge = Number(age + 7)
+
+console.log(`Em sete anos você terá ${newAge} anos`)
+
+// 2 -
+
+
